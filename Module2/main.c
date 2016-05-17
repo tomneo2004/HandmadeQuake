60f6edcd6b285811b0db6f55ba89ff5c26678e2e
NelsonPunch <tomneo2004@gmail.com>
2016-05-17 16:27:47 +0800

    2016/5/17 -Module 2 create window

---
+++ Module2/main.c
@@ -0,0 +1,49 @@
+#include <windows.h>
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int hShowCmd) {
+
+	WNDCLASS wc = { 0 };
+
+	wc.lpfnWndProc = MainWndProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszClassName = "Module 2";
+
+	if (!RegisterClass(&wc))
+		exit(EXIT_FAILURE);
+
+	HWND mainWindow;
+	DWORD WindowStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
+
+	RECT r;
+	r.top = r.left = 0;
+	r.right = 800;
+	r.bottom = 600;
+
+	AdjustWindowRect(&r, WindowStyle, FALSE);
+
+	mainWindow = CreateWindowEx(
+		0,
+		"Module 2",
+		"Lesson 2.1",
+		WindowStyle,
+		200,
+		200,
+		r.right - r.left,
+		r.bottom - r.top,
+		NULL,
+		NULL,
+		hInstance,
+		0);
+
+	HDC DeviceContext = GetDC(mainWindow);
+	PatBlt(DeviceContext, 0, 0, 800, 600, BLACKNESS);
+	ReleaseDC(mainWindow, DeviceContext);
+
+	return 0;
+}
