e10a78737484f48b6cc613fcf22ad6ba2a5facbb
NelsonPunch <tomneo2004@gmail.com>
2016-05-17 15:03:56 +0800

    2016/5/17 -Final module 1

--- HandmadeQuake/q_stl.c
+++ HandmadeQuake/q_stl.c
@@ -1,6 +1,6 @@
 #include "quakedef.h"
 
-void Q_strcpy(uint8 *dest, uint8 *src) {
+void Q_strcpy(char *dest, const char *src) {
 
 	while (*src) {
 
@@ -10,7 +10,7 @@ void Q_strcpy(uint8 *dest, uint8 *src) {
 	*dest = 0;
 }
 
-void Q_strncpy(uint8 *dest, uint8 *src, int32 count) {
+void Q_strncpy(char *dest, const char *src, int32 count) {
 
 	if (count < 0)
 		return;
@@ -28,7 +28,7 @@ void Q_strncpy(uint8 *dest, uint8 *src, int32 count) {
 	}
 }
 
-int32 Q_strlen(uint8 *str) {
+int32 Q_strlen(const char *str) {
 
 	uint32 count = 0;
 
@@ -38,7 +38,7 @@ int32 Q_strlen(uint8 *str) {
 	return count;
 }
 
-int32 Q_strcmp(uint8 *s1, uint8 *s2) {
+int32 Q_strcmp(const char *s1, const char *s2) {
 
 
 	while (*s1 == *s2) {
@@ -53,7 +53,7 @@ int32 Q_strcmp(uint8 *s1, uint8 *s2) {
 	return ((*s1 > *s2) ? -1 : 1);
 }
 
-int32 Q_atoi(uint8 *str) {
+int32 Q_atoi(const char *str) {
 
 	int32 sign = 1;
 	int32 val = 0;
